ef4cad1a6af47b6ecc1d91464c69d6cbf8ce700c
Giselle Martinez-Sanchez <53924501+GiselleMartinez006@users.noreply.github.com>
2020-09-26 18:31:49 -0400

    basic draft

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
 console.log("welcomex3")
 
 var startTime = 10
+var endTime = 1
+var timezoneArray= ["PST", "MT", "CST", "EST"]
 
-for (var i = 0; i < 3 ; i++){
-    console.log(startTime+i)
-    console.log(startTime+i< 12? "am": "pm")
-}
+function timeZoneGenerator (time, parent){
+       console.log(time)
+       console.log(parent)
+    for (var i = 0; i < 4 ; i++){
+
+        var span = document.createElement("span")
+        parent.appendChild(span)
+        span.textContent= `${time+i > 12? 0 + (i-2): time+i}${time+i > 12 || time+i < 8? "pm": "am"} ${timezoneArray[i]} ${timezoneArray[i]=== "EST"? "": " / "}`
+    
+        console.log(time+i)
+        console.log(time+i< 12? "am": "pm")
+        console.log(timezoneArray[i])
+    }
+   }
+
 
 var section = document.createElement("section")
 document.body.append(section)
 
 
 var heading = document.createElement("h1")
+heading.id= "heading"
 heading.textContent = "Office Hours @ Sundays"
 section.append(heading)
 
 var description = document.createElement("h3")
+description.id = "description"
 description.textContent = `
-From ${startTime}${startTime < 12? "am": "pm"} PST/
-     ${startTime+1}${startTime+1 < 12? "am": "pm"}  MT/
-     ${startTime+2}${startTime+2 < 12? "am": "pm"}  CST /
-     ${startTime+3}${startTime+3 < 12? "am": "pm"}  EST`
-
-
+From `
+timeZoneGenerator(startTime, description)
+description.append(document.createElement("div"))
+description.append("Until ")
+timeZoneGenerator(endTime, description)
+    // ${startTime}${startTime < 12? "am": "pm"} PST/
+    // ${startTime+1}${startTime+1 < 12? "am": "pm"}  MT/
+    // ${startTime+2}${startTime+2 < 12? "am": "pm"}  CST /
+    // ${startTime+3}${startTime+3 < 12? "am": "pm"}  EST`
+  
 
 section.append(description)

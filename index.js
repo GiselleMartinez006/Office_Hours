ba8dd687f034bfc6e96134be28e010e702c11f55
Giselle Martinez-Sanchez <53924501+GiselleMartinez006@users.noreply.github.com>
2020-09-26 10:03:51 -0400

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 console.log("welcomex3")
+
+var startTime = 10
+
+for (var i = 0; i < 3 ; i++){
+    console.log(startTime+i)
+    console.log(startTime+i< 12? "am": "pm")
+}
+
+var section = document.createElement("section")
+document.body.append(section)
+
+
+var heading = document.createElement("h1")
+heading.textContent = "Office Hours @ Sundays"
+section.append(heading)
+
+var description = document.createElement("h3")
+description.textContent = `
+From ${startTime}${startTime < 12? "am": "pm"} PST/
+     ${startTime+1}${startTime+1 < 12? "am": "pm"}  MT/
+     ${startTime+2}${startTime+2 < 12? "am": "pm"}  CST /
+     ${startTime+3}${startTime+3 < 12? "am": "pm"}  EST`
+
+
+
+section.append(description)

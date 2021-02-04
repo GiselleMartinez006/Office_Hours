8e7ba6fdf68ea6416f62fe95874eaf171b4156c2
Giselle Martinez-Sanchez <53924501+GiselleMartinez006@users.noreply.github.com>
2021-02-03 22:10:45 -0500

    where is my build folder

---
+++ app/build/precache-manifest.275716ccfb71ac0c725236637284df4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66e84048bee7dd7ec240ae020f34a58a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92adb4f1bb90d47c8bdd",
+    "url": "/static/css/main.7e81559b.chunk.css"
+  },
+  {
+    "revision": "cf9bca474aa33dc5c282",
+    "url": "/static/js/2.875f4cbc.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.875f4cbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92adb4f1bb90d47c8bdd",
+    "url": "/static/js/main.deb8a5e6.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
